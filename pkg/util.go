f72b889d680b346002ddb1791318198c9ba51fd5
Dmitry Gridnev <dmitriy.gridnev@outlook.com>
2021-09-15 22:58:01 +0300

    Test commit

--- pkg/util.go
+++ pkg/util.go
@@ -9,6 +9,7 @@ var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12
 
 func RandString(n int) string {
 	rand.Seed(time.Now().UnixNano())
+
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
